11b67bb70a7980f1848f89fbd43c02c0538791ac
Hilman Fikry <hilman12fikry@gmail.com>
2026-01-12 14:21:46 +0700

    feat: the root route

--- user-frontend/app/root.tsx
+++ user-frontend/app/root.tsx
@@ -1,7 +1,5 @@
 import {
   isRouteErrorResponse,
-  Links,
-  Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -9,19 +7,15 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import LoadingThreeDotsPulse from "./components/ui/LoadingThreeDotsPulse";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export async function HydrateFallback() {
+  return (
+    <div className="flex w-full h-full justify-center items-center">
+      <LoadingThreeDotsPulse />
+    </div>
+  );
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -29,10 +23,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
       </head>
-      <body>
+      <body className="flex flex-col min-h-screen">
         {children}
         <ScrollRestoration />
         <Scripts />

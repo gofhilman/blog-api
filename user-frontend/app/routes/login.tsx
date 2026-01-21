b4a8d3556e3f2017f531e85a80a1d92bafb06afb
Hilman Fikry <hilman12fikry@gmail.com>
2026-01-21 15:50:33 +0700

    WIP: add react router error context

--- user-frontend/app/routes/login.tsx
+++ user-frontend/app/routes/login.tsx
@@ -1,10 +1,20 @@
 import { postLogin } from "~/api/authApi";
 import type { Route } from "./+types/login";
-import { redirect } from "react-router";
+import { isRouteErrorResponse, redirect } from "react-router";
+import { errorContext } from "~/context";
 
-export async function clientAction({ params, request }: Route.ActionArgs) {
+export async function clientAction({
+  params,
+  request,
+  context,
+}: Route.ActionArgs) {
   const formData = await request.formData();
   const user = Object.fromEntries(formData);
-  await postLogin(user);
+  try {
+    await postLogin(user);
+  } catch (error: any) {
+    const message = await error.text();
+    context.set(errorContext, { code: error.status, message });
+  }
   return redirect("/" + params.postUri);
 }

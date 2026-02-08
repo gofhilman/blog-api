e81be5e4be6bd9913731d999bef1c0b28d8158d4
Hilman Fikry <hilman12fikry@gmail.com>
2026-02-09 01:22:42 +0700

    fix: move editor assets to public

--- admin-frontend/app/components/BundledEditor.tsx
+++ admin-frontend/app/components/BundledEditor.tsx
@@ -1,48 +1,10 @@
 import { Editor } from "@tinymce/tinymce-react";
-// Ensure to import tinymce first as other components expect
-// a global variable `tinymce` to exist
-import "tinymce/tinymce";
-// DOM model
-import "tinymce/models/dom/model";
-// Theme
-import "tinymce/themes/silver";
-// Toolbar icons
-import "tinymce/icons/default";
-// // Editor styles
-// import "tinymce/skins/ui/oxide-dark/skin";
-// // Content styles, including inline UI like fake cursors
-// import "tinymce/skins/content/dark/content";
-// import "tinymce/skins/ui/oxide-dark/content";
-
-// Import plugins
-import "tinymce/plugins/anchor";
-import "tinymce/plugins/advlist";
-import "tinymce/plugins/autolink";
-import "tinymce/plugins/charmap";
-import "tinymce/plugins/code";
-import "tinymce/plugins/media";
-import "tinymce/plugins/visualblocks";
-import "tinymce/plugins/fullscreen";
-import "tinymce/plugins/insertdatetime";
-import "tinymce/plugins/preview";
-import "tinymce/plugins/help";
-// Include resources that a plugin lazy-loads at the run-time
-import "tinymce/plugins/help/js/i18n/keynav/en";
-import "tinymce/plugins/image";
-import "tinymce/plugins/link";
-import "tinymce/plugins/lists";
-import "tinymce/plugins/searchreplace";
-import "tinymce/plugins/table";
-import "tinymce/plugins/wordcount";
-
-import "tinymce/plugins/codesample";
-import "tinymce/plugins/autoresize";
-import "tinymce/plugins/autosave";
 import { postImage } from "~/api/imageApi";
 
 export default function BundledEditor(props: any) {
   return (
     <Editor
+      tinymceScriptSrc="/tinymce/tinymce.min.js"
       licenseKey="gpl"
       init={{
         placeholder: "Compose your thoughts here.",
@@ -75,10 +37,10 @@ export default function BundledEditor(props: any) {
           "alignright alignjustify | bullist numlist outdent indent | " +
           "removeformat | help",
         images_upload_handler: postImage,
-        skin_url: "/app/styles/dark-zinc",
-        content_css: "/app/styles/editor-content.css",
-        // skin: "oxide-dark",
-        // content_css:"dark",
+        skin_url: "/dark-zinc",
+        content_css: "/editor-content.css",
+        // skin: "dark-zinc",
+        // content_css:"dark-zinc",
       }}
       {...props}
     />

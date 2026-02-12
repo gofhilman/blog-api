750fa5af787a81cbe9beeb4a841af475a3ad925d
Hilman Fikry <hilman12fikry@gmail.com>
2026-02-12 21:21:39 +0700

    fix: add more database connection retries

--- backend/lib/prisma.ts
+++ backend/lib/prisma.ts
@@ -10,24 +10,21 @@ const prisma = new PrismaClient({ adapter }).$extends({
   query: {
     $allModels: {
       async $allOperations({ operation, model, args, query }) {
-        return pRetry(
-          async () => {
-            try {
-              return await query(args);
-            } catch (error: any) {
-              if (error.code === "P1001") {
-                console.warn(
-                  `Retrying ${model}.${operation} due to connection error...`,
-                );
-                await prisma.$disconnect();
-                await prisma.$connect();
-                throw error;
-              }
+        return pRetry(async () => {
+          try {
+            return await query(args);
+          } catch (error: any) {
+            if (error.code === "P1001") {
+              console.warn(
+                `Retrying ${model}.${operation} due to connection error...`,
+              );
+              await prisma.$disconnect();
+              await prisma.$connect();
               throw error;
             }
-          },
-          { retries: 2 },
-        );
+            throw error;
+          }
+        });
       },
     },
   },

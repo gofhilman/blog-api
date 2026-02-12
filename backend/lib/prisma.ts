a7708416af9afef3a3845ba641a686977901d4b8
Hilman Fikry <hilman12fikry@gmail.com>
2026-02-12 17:00:16 +0700

    fix: retry database connection if failed

--- backend/lib/prisma.ts
+++ backend/lib/prisma.ts
@@ -1,10 +1,36 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+import pRetry from "p-retry";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter }).$extends({
+  query: {
+    $allModels: {
+      async $allOperations({ operation, model, args, query }) {
+        return pRetry(
+          async () => {
+            try {
+              return await query(args);
+            } catch (error: any) {
+              if (error.code === "P1001") {
+                console.warn(
+                  `Retrying ${model}.${operation} due to connection error...`,
+                );
+                await prisma.$disconnect();
+                await prisma.$connect();
+                throw error;
+              }
+              throw error;
+            }
+          },
+          { retries: 2 },
+        );
+      },
+    },
+  },
+});
 
-export { prisma }
+export { prisma };

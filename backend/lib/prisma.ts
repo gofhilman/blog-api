57df903719a0270509dd975a469bee188769741c
Hilman Fikry <hilman12fikry@gmail.com>
2026-02-17 23:03:16 +0700

    fix: reduce connection timeout limit to 1 sec

--- backend/lib/prisma.ts
+++ backend/lib/prisma.ts
@@ -7,7 +7,7 @@ const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({
   connectionString,
-  connectionTimeoutMillis: 3_000,
+  connectionTimeoutMillis: 1_000,
 });
 const prisma = new PrismaClient({ adapter }).$extends({
   query: {
